51bc869b0536a7f9416d107cd8cfd5c4de6e7d1a
Noodels79 <irubai2003@hotmail.com>
2021-10-22 11:54:26 +0200

    todos los cambios

--- Practica JS/js/saludo.js
+++ Practica JS/js/saludo.js
@@ -1,10 +1,18 @@
+var arr = new Array(0, 0, 0, 0, 0, 0)
+var a = 50
+alert("Estas son las combinaciones de la loteria de los ultimos 50 dias")
+for (s = 1; s <= a; s++) {
+    document.write("la combinacion numero " + s + " es: ")
+    for (i = 0; i < arr.length; i++) {
 
-document.write("Hola");
-var perro=2;
+        j = Math.floor((Math.random() * 48) + 1)
 
-if(perro==2){
-console.log("Hello");
+        while (arr.includes(j)) {
+            j = Math.floor((Math.random() * 48) + 1)
 
-};
-a=prompt("Escribe algo","adios")
-console.log('Has escrito'+ a)
+        }
+        arr[i] = j
+        document.write(arr[i] + " ")
+    }
+    document.write("<br>")
+}
