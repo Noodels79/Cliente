89e734629552f32969fef2e097387e0869ffee70
Noodels79 <irubai2003@hotmail.com>
2021-10-22 09:32:35 +0200

    Todo

---
+++ Practica_3.4/JS/color.js
@@ -0,0 +1,9 @@
+
+var rojo=Math.floor(Math.random()*255);
+var verde=Math.floor(Math.random()*255);
+var azul=Math.floor(Math.random()*255);
+document.write(
+    "<div id='fondo'"+
+    "style='padding:1600px; background-color:rgb("+ rojo + ',' + verde + ',' +azul +")'>"+
+    "</div>"
+);
