c5c151cdc42d6e2d25aaaca366648f4e5254426a
Noodels79 <irubai2003@hotmail.com>
2021-11-08 13:03:33 +0100

    calcuYaHecho

--- Practica_Tema_3/JS/calculadora.js
+++ Practica_Tema_3/JS/calculadora.js
@@ -1,5 +1,36 @@
-var button = document.getElementById("mybutton");
-
-class Calculadora{
-
+class Calculadora {
+    constructor() {
+        this._resul = 0;
+        this._numero1 = 0; this._numero2 = 0;
+    }
+    set resultado(accion) {
+        switch (accion) {
+            case 1:
+                this._resultado = this._numero1 + this._numero2;
+                break;
+            case 2:
+                this._resultado = this._numero1 - this._numero2;
+                break;
+        }
+    }
+    get resultado() {
+        var cadena = "El resultado es: " + this._resultado;
+        document.getElementById("salida").innerHTML = cadena;
+    }
+    datos() {
+        let x, y;
+        x = parseInt(prompt("Dame el primer operando: ", "0"));
+        y = parseInt(prompt("Dame el segundo operando", "0"));
+        if (isNaN(x && y) == false) {
+            this._numero1 = x;
+            this._numero2 = y;
+            
+        } else prompt("No son números", "0");
+    }
+}
+function programa(operacion) {
+    var objCalcula = new Calculadora();
+    objCalcula.datos();
+    objCalcula.resultado = operacion;
+    objCalcula.resultado;
 }
