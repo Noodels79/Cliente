6e61db6b5cd99b400347f198b2a6fae79af3770f
Noodels79 <irubai2003@hotmail.com>
2021-11-22 12:34:05 +0100

    xxxx

---
+++ 2ºTrimestre/Practica_8_1/JS/accion.js
@@ -0,0 +1,6 @@
+document.body.addEventListener("mousemove", function(ev){
+    let cHola=document.getElementById("cHola");
+    cHola.style.left=ev.clientX+"px";
+    cHola.style.top=ev.clientY+"px";
+
+});
